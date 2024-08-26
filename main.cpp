a4a051d579aa36d2300a310ac499e789c81dd406
Shrasti Agrawal <128316597+Shrasti12@users.noreply.github.com>
2024-08-26 15:06:45 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
+#include <bits/stdc++.h>
+#include "parser.h"
 
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+/** Global Constants **/
+const string TRACE = "trace";
+const string SHOW_STATISTICS = "stats";
+const string ALGORITHMS[9] = {"", "FCFS", "RR-", "SPN", "SRT", "HRRN", "FB-1", "FB-2i", "AGING"};
