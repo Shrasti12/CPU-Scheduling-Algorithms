b5c91c305238ff2dc23bdd2c2fa82de6c53a4841
Shrasti Agrawal <128316597+Shrasti12@users.noreply.github.com>
2024-08-26 15:26:18 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,3 +9,17 @@ using namespace std;
 const string TRACE = "trace";
 const string SHOW_STATISTICS = "stats";
 const string ALGORITHMS[9] = {"", "FCFS", "RR-", "SPN", "SRT", "HRRN", "FB-1", "FB-2i", "AGING"};
+
+bool sortByServiceTime(const tuple<string, int, int> &a, const tuple<string, int, int> &b)
+{
+    return (get<2>(a) < get<2>(b));
+}
+bool sortByArrivalTime(const tuple<string, int, int> &a, const tuple<string, int, int> &b)
+{
+    return (get<1>(a) < get<1>(b));
+}
+
+bool descendingly_by_response_ratio(tuple<string, double, int> a, tuple<string, double, int> b)
+{
+    return get<1>(a) > get<1>(b);
+}
